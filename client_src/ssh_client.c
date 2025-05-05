a640217e35b29dd4b9456ff6f955ccc99d3ab828
Yellowly <irawizza@gmail.com>
2025-05-05 17:16:38 -0400

    Fixed minor syntax errors

--- client_src/ssh_client.c
+++ client_src/ssh_client.c
@@ -1,28 +1,30 @@
 #include <netinet/in.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <stdatomic.h>
 
-atomic_int flag = 0; // For thread coordination
+int flag = 0; // For thread coordination
 
 void *reader(void *arg) {
-  int socket_fd = *(int *) arg;
+  int socket_fd = *(int *)arg;
   char buffer[1024];
 
   while (!flag) {
     ssize_t bytes_read = read(socket_fd, buffer, sizeof(buffer) - 1);
-
     if (bytes_read <= 0) {
       flag = 1;
       break;
     }
 
     buffer[bytes_read] = '\0';
+    buffer[bytes_read - 1] = '\0';
 
-    // This stores what's written in the socket fd  and prints to terminal, but idk what else it needs to do
+    // This stores what's written in the socket fd  and prints to terminal, but
+    // idk what else it needs to do
     write(STDOUT_FILENO, buffer, bytes_read);
+    fsync(STDOUT_FILENO);
   }
 
   return NULL;
@@ -45,7 +47,8 @@ int connect_client(struct sockaddr_in *addr) {
   }
 
   // Connect to server
-  if (connect(socket_fd, addr, sizeof(struct sockaddr_in)) < 0) {
+  if (connect(socket_fd, (struct sockaddr *)addr, sizeof(struct sockaddr_in)) <
+      0) {
     perror("Error connecting to server\n");
     close(socket_fd);
     return -1;
@@ -64,8 +67,8 @@ int connect_client(struct sockaddr_in *addr) {
 int run_client(struct sockaddr_in addr) {
   int socket_fd = connect_client(&addr);
 
-  dup2(socket_fd, STDIN_FILENO);
-  dup2(socket_fd, STDOUT_FILENO);
+  // dup2(socket_fd, STDIN_FILENO);
+  // dup2(socket_fd, STDOUT_FILENO);
 
   printf("Connected to server! Type 'exit' to quit.\n");
 
@@ -75,25 +78,23 @@ int run_client(struct sockaddr_in addr) {
 
   pthread_create(&tid, NULL, reader, &socket_fd);
 
-  char buffer[1024];
-  while (flag) {
-    if (!fgets(buffer, sizeof(buffer), STDIN_FILENO)) {
+  // char buffer[1024];
+  while (!flag) {
+    if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
       flag = 1;
       break;
     }
 
     // Checks for exit command, quit if so
-    if (!strncmp(buffer, "exit", 4)) {
-      flag = 1;
-      break;
-    }
+    // if (!strncmp(buffer, "exit", 4)) {
+    //  flag = 1;
+    //  break;
+    //}
 
     write(socket_fd, buffer, strlen(buffer));
   }
-
-  pthread_join(reader, NULL);  
+  pthread_join(tid, NULL);
   close(socket_fd);
-
 
   printf("Client was closed\n");
   return 0;
